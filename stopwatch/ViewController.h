b83ef47a45c3ff9e9e53f514e0e67b9496519210
Oğuz Bayat <oguzbayat@Oguz-MacBook-Pro.local>
2017-07-19 18:13:09 +0300

    codes

--- stopwatch/ViewController.h
+++ stopwatch/ViewController.h
@@ -9,6 +9,20 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    NSTimer *myTimer;
+    BOOL running;
+    int count;
+}
+
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *resetBtn;
+@property (weak, nonatomic) IBOutlet UIButton *startBtn;
+
+- (IBAction)startBtnPushed:(id)sender;
+- (IBAction)resetBtnPushed:(id)sender;
+
+- (void)updateTimer;
 
 
 @end
--- stopwatch/ViewController.m
+++ stopwatch/ViewController.m
@@ -13,10 +13,17 @@
 @end
 
 @implementation ViewController
+@synthesize timerLabel,startBtn,resetBtn;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    running = NO;
+    count = 0;
+    timerLabel.text = @"00:00.00";
+    
+    startBtn.layer.cornerRadius = 50;
+    resetBtn.layer.cornerRadius = 50;
 }
 
 
@@ -25,5 +32,52 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)startBtnPushed:(id)sender {
+    if (running == NO) {
+        running = YES;
+        [startBtn setTitle:@"STOP" forState:UIControlStateNormal];
+        
+        if (myTimer == nil) {
+            
+            myTimer = [NSTimer scheduledTimerWithTimeInterval:0.01 target:self selector:@selector(updateTimer) userInfo:nil repeats:YES];
+            
+        }
+        
+    } else {
+        [self stopTimer];        
+    }
+}
+
+- (IBAction)resetBtnPushed:(id)sender {
+    [self stopTimer];
+    
+    count = 0;
+    timerLabel.text = @"00:00.00";
+}
+
+- (void)stopTimer {
+    
+    running = NO;
+    [myTimer invalidate];
+    myTimer = nil;
+    [startBtn setTitle:@"START" forState:UIControlStateNormal];
+    
+}
+
+- (void)updateTimer {
+    
+    count++;
+    
+    int min = floor(count/100/60);
+    int sec = floor(count/100);
+    int mSec = count % 100;
+    
+    if (sec >= 60) {
+        sec = sec % 60;
+    }
+    
+    timerLabel.text = [NSString stringWithFormat:@"%02d:%02d.%02d",min,sec,mSec];
+}
 
 @end
